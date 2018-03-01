b20b85b6ed260329546480843e9ac0009641a31a
jeruliu <jeru.liu@yahoo.com>
2018-03-01 18:43:15 +0800

    Create bg_sandbox.py

---
+++ bg_sandbox.py
@@ -0,0 +1,29 @@
+from imutils import build_montages
+from imutils import paths
+import cv2
+import imutils
+import bg_detector as bg
+
+targetFolder = "D:/dev/bg"
+
+images = []
+
+for imagePath in paths.list_images(targetFolder):
+    #load the image and resize it to speed up the computation
+    image = cv2.imread(imagePath)
+    image = imutils.resize(image, width=250)
+    #edge = bg.canny_edge(image)
+    #cv2.rectangle(edge,(0,0),(edge.shape[1],edge.shape[0]),(255, 255, 255),3)
+    message = bg.detect_bg(image, 20)
+    cv2.putText(image, message, (40, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
+    images.append(image)
+
+# display 25 product pictures in 5X5 matrix   
+originMontages = build_montages(images, (128,162), (5,5))
+#originMontages = build_montages(images, (128, 162), (5,5))
+
+# loop over the montages and display each of them
+for montage in originMontages:
+	cv2.imshow("Product Album", montage)
+	cv2.waitKey(0)
+   
